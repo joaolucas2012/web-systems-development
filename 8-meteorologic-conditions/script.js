25c1c0bd6ca2cd4a5a7ec7c7dc2f6deb404da835
unknown <ribeirojoaolucas68@gmail.com>
2021-06-04 16:27:00 -0300

    finished the exercise 8

--- 8-meteorologic-conditions/script.js
+++ 8-meteorologic-conditions/script.js
@@ -1,45 +1,51 @@
 $(document).ready(function () {
-  $("btnWeather").click(buscarInformacoes)
+  $("#btnWeather").click(searchInfo)
 });
 
-buscarCidade = function (cidade) {
+//Function to search the city
+searchCity = function (city) {
   return new Promise(function (resolve, reject) {
-    let resultado;
-    if (typeof cidade == 'string') {
-      resultado = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=10e2c4ba8f02d372543f3f834fbcf33d");
+    let answer;
+    if (typeof city == 'string') {
+      answer = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=10e2c4ba8f02d372543f3f834fbcf33d");
     } else {
       reject();
     }
 
-    if (resultado) {
-      resolve(resultado);
+    if (answer) {
+      resolve(answer);
     } else {
       reject();
     }
   });
 }
 
-colocarNoHtml = function (info) {
+//function to place the result into the HTML
+placeIntoHtml = function (info) {
   const climaAtual = info.weather[0].description;
   const temperaturaAtual = (info.main.temp - 273.15).toFixed(2);
   const temperaturaMinima = (info.main.temp_min - 273.15).toFixed(2);
   const temperaturaMaxima = (info.main.temp_max - 273.15).toFixed(2);
 
-  $("#resultado").html(
-    `<p>Condição Atual: ${climaAtual}</p>
-      <p>Temperatura Atual: ${temperaturaAtual}</p>
-      <p>Temperatura máxima: ${temperaturaMaxima}</p>
-      <p>Temperatura minima: ${temperaturaMinima}</p>
-      `
+  //showing the information 
+  $("#result").html(
+    `<p><b>Condição Atual: ${climaAtual}</b></p>
+        <p><b>Temperatura Atual: ${temperaturaAtual} °C</b></p>
+        <p><b>Temperatura máxima: ${temperaturaMaxima} °C</b></p>
+        <p><b>Temperatura minima: ${temperaturaMinima}° C</b></p>
+        <br><br>
+        <p>&copy; 2021 João Lucas. All rights reserved.</p>
+        `
   );
 }
 
-function buscarInformacoes() {
-  let cidade = $("input").val();
-
-  buscarCidade(cidade).then(colocarNoHtml).catch((err) => {
-    return $("#resultado").html(
-      `<p id="error">Cidade não encontrada</p>`
+//function to search the necessary information
+function searchInfo() {
+  let city = $("input").val();
+  //search the city typed by the user
+  searchCity(city).then(placeIntoHtml).catch((err) => {
+    return $("#result").html(
+      `<p id="error">Cidade não encontrada!</p>`
     );
   });
 }
